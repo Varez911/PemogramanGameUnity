36ede78ea1c43f69d7556f61c9d037db18811536
Varez911 <varez.yeyez@gmail.com>
2020-10-09 13:17:54 +0700

    Adding Parallax in Scene Percobaan P2

---
+++ 2D Pertemuan 1/Assets/Script/Paralax.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    public List<Transform> backgrounds = new List<Transform>();
+    public float movementSpeed;
+
+    private Transform cam;
+    private float[] skalaParalax;
+    private Vector3 prevPosCam;
+
+
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    void Start()
+    {
+        prevPosCam = cam.position;
+
+        skalaParalax = new float[backgrounds.Count];
+
+        for (int i = 0; i < backgrounds.Count; i++)
+        {
+            skalaParalax[i] = backgrounds[i].position.z * -1;
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < backgrounds.Count; i++)
+        {
+            float parallax = (prevPosCam.x - cam.position.x) * skalaParalax[i];
+            float BGTargetPosX = backgrounds[i].position.x + parallax;
+            Vector3 BGNewPos = new Vector3(BGTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, BGNewPos, movementSpeed * Time.deltaTime);
+        }
+        prevPosCam = cam.position;
+    }
+}
